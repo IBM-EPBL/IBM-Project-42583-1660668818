8bac190ebba713739ac31e5a89dacd00e4333f2e
Sherin2001 <107489882+Sherin2001@users.noreply.github.com>
2022-11-06 09:28:19 -0800

    Add files via upload

---
+++ Assignments/Assignment-1/Team member 1/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("registration.html")
+@app.route("/confirm",methods=['POST','GET'])
+def registration():
+    if request.method == 'POST':
+        n =request.form.get('username')
+        e =request.form.get('email')
+        p =request.form.get('phone')
+        return render_template('display.html',username=n,email=e,phone=p)
+    
+
+if __name__=="__main__":
+    app.run(debug=True)
